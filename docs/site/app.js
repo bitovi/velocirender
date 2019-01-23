f6685ce8cafd5f2660a86d495dde7c5b770b7f0b
Matthew Phillips <matthew@matthewphillips.info>
2019-01-23 14:41:22 -0500

    Get the tabs kinda working

--- docs/site/app.js
+++ docs/site/app.js
@@ -6,5 +6,18 @@ hljs.initHighlighting();
 tabs();
 
 function tabs() {
+  let tabs = document.querySelector('.tabs');
+  tabs.addEventListener('click', ev => {
+    if(ev.target.localName === 'button') {
+      let parentLi = ev.target.parentNode;
 
+      for(let li of tabs.children) {
+        if(li === parentLi) {
+          li.classList.add('selected');
+        } else {
+          li.classList.remove('selected');
+        }
+      }
+    }
+  })
 }

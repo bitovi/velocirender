b490c89415f089a0aa0e606f755e379d49e1ee19
Matthew Phillips <matthew@matthewphillips.info>
2019-02-08 08:42:24 -0500

    Verify that MutationObserver works
    This makes sure code using MO works. Closes #53

--- test/cli_test.js
+++ test/cli_test.js
@@ -20,7 +20,11 @@ describe("CLI", function() {
 		})
 
 		it('Renders a page', async function() {
-			let res = await fetch('http://localhost:8055');
+			let res = await fetch('http://localhost:8055', {
+				headers: {
+					"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+				}
+			});
 			let html = await res.text();
 
 			assert.ok(/<main>/.test(html), "Rendered with the initial HTML");
